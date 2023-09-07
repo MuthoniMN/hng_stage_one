780d1017870200a078d4b6a30b6a4ca6d8867a41
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-07 15:40:46 +0300

    adding date and time

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let dateTime = new Date()
+
+let day = dateTime.toString().split(' ')[0]
+document.querySelector("#day").textContent = day
+let time = dateTime.toUTCString().split(' ')[4]
+document.querySelector('#time').textContent = time
