47e2db6e96bb80d026619b179b17a16a6aebaa96
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-09-09 12:37:24 +0300

    revision

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let dateTime = new Date()
 
 let day = dateTime.toString().split(' ')[0]
 document.querySelector("#day").textContent = day
-let time = dateTime.toUTCString().split(' ')[4]
-document.querySelector('#time').textContent = time + " UTC"
+let time = dateTime.getUTCMilliseconds()
+document.querySelector('#time').textContent = time
